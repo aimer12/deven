aaaedfe0ee81fec68c6fd78fef5d91d66c49fc61
DWDeven <592853337@qq.com>
2016-09-13 17:34:03 +0800

    FIx Bug

--- clock.bat
+++ clock.bat
@@ -7,7 +7,7 @@ set/p option=输入分钟数:
 goto deal
 
 :deal
-if "%option%" gtr "0" (if "%option%" lss "45" (goto clock) else goto time_too_long) else goto input_error
+if %option% geq 1 (if %option% leq 45 (goto clock) else goto time_too_long) else goto input_error
 
 :time_too_long
 echo 要听宝宝的建议，不能连续工作超过45分钟！
@@ -18,15 +18,15 @@ echo 不要调戏我了，宝宝建议输入45分钟以下哦！
 goto rechose
 
 :clock
-echo 开始为宝宝倒计时工作后休息！
-ping -n 2 127.1>nul
-if "%1" == "h" goto begin 
-　　mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+echo 开始为宝宝倒计时工作后休息!
+goto begin
 
-:begin 
-ping -n %option%*60 127.1>nul
+:begin
+set /a option = %option%*60
+ping -n %option% 127.1>nul
 mshta vbscript:createobject("sapi.spvoice").speak("宝宝，你需要休息一下了")(window.close)
 mshta vbscript:msgbox("宝宝，你该休息一下了!",36,"来自爱你的望望")(window.close)
+exit
 
 :rechose
 set/p option=乖~输入合适的工作时间:
